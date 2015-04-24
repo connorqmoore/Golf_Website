20551592974491fc6360712f5b8efec85ae5aae7
Connor Moore <cqm@andrew.cmu.edu>
2015-04-24 17:25:07 -0400

    finished up

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+// $(document).foundation();
+$(document).ready(function() {
+  $("#a_about").click(function( event ) {
+    $('#about').ScrollTo();
+  });
+
+  $("#a_products").click(function( event ) {
+    $('#products').ScrollTo();
+  });
+
+  $("#a_contact").click(function( event ) {
+    $('#contact').ScrollTo();
+  });
+
+});
